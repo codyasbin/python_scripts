df17b464844ca140e3e2fd7c72415f5bdea82553
codyasbin <himalthapa346@gmail.com>
2026-02-02 22:10:34 +0545

    duplex channel formed

--- p2p_tcp_server.py
+++ p2p_tcp_server.py
@@ -1,37 +1,39 @@
 import socket
+import threading
 
+HOST = "192.168.1.70"
+PORT = 5000
 
+def receive_messages(conn):
+    while True:
+        try:
+            data = conn.recv(1024)
+            if not data:
+                print("\nClient disconnected")
+                break
+            print(f"\nClient: {data.decode()}\nYou: ", end="")
+        except:
+            break
 
-# Example: Get your computer's hostname
-hostname = socket.gethostname()
-print(f"Hostname: {hostname}")
-
-# Example: Get IP address
-ip_address = "192.168.1.70"
-print(f"IP Address: {ip_address}")
-
-port =5000
-
-# Create a TCP socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket to the address and port
-server_socket.bind((ip_address, port))
-
-# Listen for incoming connections
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((HOST, PORT))
 server_socket.listen(1)
-print(f"Listening on {ip_address}:{port}...")
 
-# Accept a connection
-client_socket, client_address = server_socket.accept()
-print(f"Connection from {client_address} has been established!")
+print(f"Server listening on {PORT}...")
 
-# Receive data from the client
-# 5. Receive data loop
+conn, addr = server_socket.accept()
+print(f"Connected to {addr}")
+
+# Start receive thread
+threading.Thread(target=receive_messages, args=(conn,), daemon=True).start()
+
+# Send loop
 while True:
-    data = client_socket.recv(1024)
-    if not data:
+    msg = input("You: ")
+    if msg.lower() == "exit":
         break
-    print("Client:", data.decode())
+    conn.send(msg.encode())
 
-client_socket.close()    
+conn.close()
+server_socket.close()
