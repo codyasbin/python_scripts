4ad47522058a783b798151c2f18a342ed9eaab69
codyasbin <himalthapa346@gmail.com>
2026-02-02 21:34:34 +0545

    tcp connection script added

--- p2p_tcp.py
+++ p2p_tcp.py
@@ -12,4 +12,26 @@ print(f"IP Address: {ip_address}")
 
 port =5000
 
+# Create a TCP socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the address and port
+server_socket.bind((ip_address, port))
+
+# Listen for incoming connections
+server_socket.listen(1)
+print(f"Listening on {ip_address}:{port}...")
+
+# Accept a connection
+client_socket, client_address = server_socket.accept()
+print(f"Connection from {client_address} has been established!")
+
+# Receive data from the client
+# 5. Receive data loop
+while True:
+    data = client_socket.recv(1024)
+    if not data:
+        break
+    print("Client:", data.decode())
+
+client_socket.close()    
