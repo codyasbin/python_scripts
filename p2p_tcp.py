33d5c6c88b65e8dd1c5a65e5588a31b0b56b741f
codyasbin <himalthapa346@gmail.com>
2026-02-02 21:32:25 +0545

    server socket added

--- p2p_tcp.py
+++ p2p_tcp.py
@@ -1,5 +1,7 @@
 import socket
 
+
+
 # Example: Get your computer's hostname
 hostname = socket.gethostname()
 print(f"Hostname: {hostname}")
@@ -7,3 +9,7 @@ print(f"Hostname: {hostname}")
 # Example: Get IP address
 ip_address = socket.gethostbyname(hostname)
 print(f"IP Address: {ip_address}")
+
+port =5000
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
