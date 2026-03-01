bb96182b984ceea522c219a766aeda157557eebe
codyasbin <himalthapa346@gmail.com>
2026-03-01 14:06:03 +0545

    adding arguments to the terminal command

--- CLI_tool.py
+++ CLI_tool.py
@@ -1,18 +1,28 @@
-# user_cli.py
+import sys
 import requests
 
 API_URL = "https://randomuser.me/api/"
 
+# 1. Read CLI argument
+arg = sys.argv[1] if len(sys.argv) > 1 else "all"
+
+# 2. Call public API
 response = requests.get(API_URL)
-
 data = response.json()
-
 user = data["results"][0]
 
 name = f"{user['name']['first']} {user['name']['last']}"
 email = user["email"]
 country = user["location"]["country"]
 
-print("👤 Name   :", name)
-print("📧 Email  :", email)
-print("🌍 Country:", country)
+# 3. Decide output based on CLI argument
+if arg == "name":
+    print(name)
+elif arg == "email":
+    print(email)
+elif arg == "country":
+    print(country)
+else:
+    print("Name   :", name)
+    print("Email  :", email)
+    print("Country:", country)
