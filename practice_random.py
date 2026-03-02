8127d5807d6e8e034c452453f897889c55166b16
codyasbin <himalthapa346@gmail.com>
2026-03-02 21:27:57 +0545

    updates

--- practice_random.py
+++ practice_random.py
@@ -1,2 +1,10 @@
-print ("Welcome to the asbins game!")
-number=int(input("Enter a number: "))
+print ("Press Enter to start the game!\n Press Ctrl+C to exit the game.")
+if input() == "":
+ while True:
+    print ("Welcome to the asbins game!")
+    number=int(input("Enter a number: "))
+    if number%2==0:
+        print ("You win!")
+    else:        print ("You lose!")
+    print ("Press Enter to play again or Ctrl+C to exit the game.")
+    input()
