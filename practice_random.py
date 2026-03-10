8d5e6274a3be312b575145e1db9e1a00561963f0
codyasbin <himalthapa346@gmail.com>
2026-03-10 16:10:57 +0545

    practice

--- practice_random.py
+++ practice_random.py
@@ -1,18 +1,22 @@
-print ("Press Enter to start the game!\n Press Ctrl+C to exit the game.")
-if input() == "":
- while True:
-    print ("Welcome to the asbins game!")
-    number=int(input("Enter a number: "))
-    if number%2==0:
-        for i in range(3):
-            print ("Asbin")
-        print ("You win!")
-    else:
-        print ("You lose!")
-    print ("Press Enter to play again!\nPress Ctrl+C to exit the game.")
-    if input().lower()=="q":
-        break
+# print ("Press Enter to start the game!\n Press Ctrl+C to exit the game.")
+# if input() == "":
+#  while True:
+#     print ("Welcome to the asbins game!")
+#     number=int(input("Enter a number: "))
+#     if number%2==0:
+#         for i in range(3):
+#             print ("Asbin")
+#         print ("You win!")
+#     else:
+#         print ("You lose!")
+#     print ("Press Enter to play again!\nPress Ctrl+C to exit the game.")
+#     if input().lower()=="q":
+#         break
 
-if input().lower()=="q":
-    print ("Thanks for playing!")
+# if input().lower()=="q":
+#     print ("Thanks for playing!")
 
+variable:str= "sdc"
+number:int= 5
+print (type(variable))
+print (type(number))
