b69978cd464fc7612f55756cb7f17347e4b9b4ba
codyasbin <himalthapa346@gmail.com>
2026-03-17 14:07:24 +0545

    comment added

--- decorators/decorator_example.py
+++ decorators/decorator_example.py
@@ -15,3 +15,5 @@ print (greet(name))
 def sir_name():
     sir_name="magar"
     return sir_name
+
+# decorators are a powerful tool in Python that allow you to modify the behavior of functions or classes. They are often used to add functionality to existing code without modifying the original code directly. In this example, we have a simple decorator called `greet` that takes a function as an argument and prints the result of calling that function. The `name` function is defined to return a string, and when we call `greet(name)`, it executes the `name` function and prints the result.
