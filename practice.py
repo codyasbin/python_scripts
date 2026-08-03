f8c3d1caacfa536563de0e926d22f64cc8ffbb17
codyasbin <himalthapa346@gmail.com>
2026-08-03 13:58:34 +0545

    practice

--- practice.py
+++ practice.py
@@ -1,5 +1,12 @@
-a:str="10"
-b:str="20"
-c:str=a+b
-print(c)
-print(type(a))
+# import sys  
+
+# sys is a module that provides access to some variables used or maintained by the interpreter and to functions that interact strongly with the interpreter. It is always available. basically a interpreter information as a module
+
+# print(sys.version)
+# print(sys.version_info)
+# print(sys.path)
+# print(sys.executable)
+# print(sys.platform)
+# print(sys.maxsize)
+
+"""This is multiple line comment"""
